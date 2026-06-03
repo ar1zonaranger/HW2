55b77c44dbbee9342b24bf97a046b45fd30ebbfa
Эльдар Тагиров <tagiroved@mail.ru>
2026-06-03 18:55:32 +0300

    Исправлены названия атрибутов

--- Ingredient.py
+++ Ingredient.py
@@ -1,27 +1,25 @@
 class Ingredient:
     def __init__(self, name, quantity, unit):
-        self._name = name
-        self._quantity = quantity
-        self._unit = unit
+        self.name = name
+        self.quantity = quantity
+        self.unit = unit
 
     @property
     def quantity(self):
-        return self._quantity
+        return self.quantity
     
     @property.setter
     def quantity(self, value):
         if value < 0:
             raise ValueError("Количество должно быть положительным.")
-        self._quantity = float(value)
+        self.quantity = float(value)
 
     def __str__(self):
-        return f"{self._name}: {self._quantity} {self._unit}"
+        return f"{self.name}: {self.quantity} {self.unit}"
 
     def __repr__(self):
-        return f"Ingredient(\'{self._name}\', {self._quantity}, \'{self._unit}\')"
+        return f"Ingredient(\'{self.name}\', {self.quantity}, \'{self.unit}\')"
     
     def __eq__(self, other):
         return (isinstance(other, Ingredient) and
-                self._name == other._name and self._unit == other._unit)
-    
-    
+                self.name == other.name and self.unit == other.unit)
