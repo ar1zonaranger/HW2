79e5c680f7070a22072ca03163e480b6d200d36f
Эльдар Тагиров <tagiroved@mail.ru>
2026-06-03 18:55:59 +0300

    Реализован класс Recipe

---
+++ Recipe.py
@@ -0,0 +1,31 @@
+import Ingredient
+
+class Recipe:
+    def __init__(self, title, ingredients):
+        self.title = title
+        self.ingredients = ingredients
+
+    def add_ingredient(self, ingredient : Ingredient):
+        for i in self.ingredients:
+            if i == ingredient:
+                i.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        return isinstance(ratio, (int, float)) and ratio > 0
+    
+    def ratio(self, ratio: float):
+        if not self.is_valid_ratio(ratio):
+            raise ValueError("Коэффициент должен быть положительным.")
+        newRecipe = Recipe(self.title, [])
+        for ingredient in self.ingredients:
+            newRecipe.add_ingredient(Ingredient(ingredient.name, ingredient.quantity*ratio, ingredient.unit))
+        return newRecipe
+    
+    def __len__(self):
+        return len(self.ingredients)
+    
+    def __str__(self):
+        return f"{self.title}:\n" + "\n".join(str(i) for i in self.ingredients)
